10af69998cd980773ee1f1aede9758350ddc8989
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2021-04-15 17:59:04 +0000

    Home page with some content

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,25 @@
+import Title from "../utils/title/Title";
+
 function Home() {
   return (
-    <div className="ct container-fluid">
-      <div className="row h-100">
-        Welcome!
+    <div className="ct container">
+      <div className="row">
+        <div className="col">
+          <Title title="Bienvenido!" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <p>
+            Esta es la sección académica, aqui podrás encontrar algunas guías para entender mejor algún tema de tu interés.
+          </p>
+          <p>
+            Aprender lo escencial para nuevas tecnologías ó para poder ponerte al día en algunas materias de la universidad.
+          </p>
+          <p>
+            Esperamos que este proyecto sea de tu agrado y te pueda ayudar en tu camino al éxito :)
+          </p>
+        </div>
       </div>
     </div>
   );
