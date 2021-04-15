64754d0270d2ec7e520bd8da43ae52ec49aff41b
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2021-04-15 17:49:40 +0000

    not found page fixed

--- src/components/utils/not-found/NotFound.tsx
+++ src/components/utils/not-found/NotFound.tsx
@@ -3,16 +3,14 @@ import { Link, useHistory } from "react-router-dom";
 function NotFound() {
   const history = useHistory();
   return (
-    <div className="ct container-fluid">
-      <div className="row h-100">
-        <h3>Not found</h3>
-        <Link to="/" className="btn btn-outline-dark">
-          Go to page home
+    <div className="ct">
+      <h3>Not found</h3>
+      <Link to="/" className="btn btn-outline-dark">
+        Go to page home
       </Link>
-        <button onClick={goBack} className="btn btn-outline-dark">
-          Go back
+      <button onClick={goBack} className="btn btn-outline-dark">
+        Go back
       </button>
-      </div>
     </div>
   );
   function goBack() {
